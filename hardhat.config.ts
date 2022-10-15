58481703e50ffe2a047cf691e9cd4c9330310914
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-10-15 23:26:15 +0100

    testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,63 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+/* global task ethers */
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+// import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+require("hardhat-contract-sizer");
+require("dotenv").config();
+require("solidity-coverage");
+
+// You have to export an object to set up your config
+// This object can have the following optional entries:
+// defaultNetwork, networks, solc, and paths.
+// Go to https://buidler.dev/config/ to learn more
+module.exports = {
+  mocha: {
+    timeout: 100000000,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 20000000,
+      timeout: 120000,
+      gas: "auto",
+    },
+    localhost: {
+      timeout: 8000000,
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      blockGasLimit: 200000000000,
+      gasPrice: 10000000000,
+      timeout: 90000,
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 100,
+    enabled: false,
+  },
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: false,
+    disambiguatePaths: true,
+  },
+  // This is a sample solc configuration that specifies which version of solc to use
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
-
-export default config;
